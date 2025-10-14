48ae19405bbd8b6520c4d3ac7dda05e609e1d8ef
weslleysoares <correweslleysoares@gmail.com>
2025-10-14 23:10:39 +0100

    Adicionando Administrador e DbContext

--- Infraestrutura/Db/DbContext.cs
+++ Infraestrutura/Db/DbContext.cs
@@ -1,5 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using MinhaMinimalApi.Dominio.Entidades;
+
 namespace MinimalApi.Infraestrutura.Db;
-class DbContexto
+
+public class DbContexto : DbContext
 {
-    
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(
+            "String de conexão com o banco de dados",
+            ServerVersion.AutoDetect("String de conexão com o banco de dados")
+
+        );
+    }
 }
